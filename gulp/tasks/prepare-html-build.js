172e2c731a821aff397653bf1c9f3ab7e877f8f4
Eugene <eugene.kotsarev@gmail.com>
2021-08-04 20:50:29 +0300

    fix link to build pages

--- gulp/tasks/prepare-html-build.js
+++ gulp/tasks/prepare-html-build.js
@@ -66,7 +66,7 @@ module.exports = () => {
         <li class="main__item">
           <article class="main__article">
             <h2 class="main__title">${pages[pageName].title}</h2>
-            <a class="${pages[pageName].image === undefined ? 'main__link main__link--default' : 'main__link'}" href="${pageName}.html" title="${pages[pageName].title}" aria-label="Link to ${pages[pageName].title} page.">
+            <a class="${pages[pageName].image === undefined ? 'main__link main__link--default' : 'main__link'}" href="./html/${pageName}.html" title="${pages[pageName].title}" aria-label="Link to ${pages[pageName].title} page.">
               <img src="./${$.config.metaPath}/${pages[pageName].image === undefined ? '1000_default.svg' : pages[pageName].image}" alt="Preview image for ${pages[pageName].title}" loading="lazy">
             </a>
           </article>
